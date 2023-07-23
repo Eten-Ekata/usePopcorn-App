7550953360e1aae9c58480134f7e1257e7c331c0
Eten-Ekata <ekataelishaeten@gmail.com>
2023-07-23 13:15:54 +0100

    style setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './index.css'
 
 function App() {
 
   return (
     <>
-      <h1>eten</h1>
+      <h1>IMADE</h1>
     </>
   )
 }
